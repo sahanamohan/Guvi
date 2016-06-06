2216b3ef4632a27c7f17843ec4cbeec8474a52e2
sahanamohan <sahanarajmohan75@gmail.com>
2016-06-06 20:23:12 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i=1;
+	while(i<=5){
+	     System.out.println("Hello"); i++;  }
+	}
+}
